e2951ccb4601a0172c563e6380647e1f4e445a26
Piotrek <piotrek.procner@gmail.com>
2014-12-14 22:45:27 +0100

    Added retrieving uuid.

--- piotrek.rb
+++ piotrek.rb
@@ -1,3 +1,10 @@
 #!/usr/bin/ruby
 
-puts "Hello, Ruby!"
+require 'rexml/document'
+include REXML
+
+xmlfile = File.new("item.xml")
+xmldoc = Document.new(xmlfile)
+root = xmldoc.root
+
+puts "UUID: " + root.attributes["uuid"]
